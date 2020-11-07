f02013afc5b9a909dbee8dbb1c912f526d10eefe
Anirudh Srikanth <anirudhs1998@gmail.com>
2020-11-07 16:32:17 +0530

    Added POST,PUT,PATCH AND DELETE methods to our APIAView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
+
 
 class HelloAPIView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
+
+
 
     def get(self, request, format=None):
         """Return a list of APIView features"""
@@ -14,3 +20,37 @@ class HelloAPIView(APIView):
         ]
 
         return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data = request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name') #Retrieve any field we defined in the serializers
+            message = f'Hello {name}' #Using f function to use variable in a string
+
+            return Response({'message':message})
+
+        else:
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST
+            ) #1st term gives a dictionary of all the errors based on the validation rules of the serializer
+
+
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+
+        return Response({'method': 'PUT'})
+
+
+    def patch(self,request,pk=None):
+        """Handle partial update of an object"""
+
+        return Response({'method':'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+
+        return Response({'method': 'DELETE'})
